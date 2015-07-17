f907d71df57452452ed6f47ccd7b912f8eda3aa8
Seth Keefer <keeferseth132@gmail.com>
2015-07-17 08:02:11 -0600

    midterm demo

--- midterm-suggestions.js
+++ midterm-suggestions.js
@@ -2,9 +2,16 @@ $(document).on('ready', function(){
 
 	$('#send-button').on('click', function(){
 		var inputValue = $('#input-box').val()
+		$('#input-box').val('')
 
 		$('.container').append(inputValue)
 	})
 
+	$('#input-box').keypress(function (e) {
+	  if (e.which === 13) {
+	    $('#send-button').click();
+	    return false;   
+	  }
+	});
 
 })
